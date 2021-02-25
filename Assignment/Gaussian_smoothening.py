de163d21d2ab98c52dc9c5a4542ec4e6c05f547a
Abhinanda Dash <71942917+Abhi-mujhemein-kahin@users.noreply.github.com>
2021-02-26 00:42:48 +0530

    Image+Histogram with sigma=10

--- Assignment/Gaussian_smoothening.py
+++ Assignment/Gaussian_smoothening.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from scipy import misc, ndimage
+
 brain = plt.imread("Brain.jpg")
 brain.shape
 plt.imshow(brain, cmap='Greys_r')
@@ -7,7 +8,14 @@ plt.show()
 plt.hist(brain, bins = 10)
 plt.show()
 least_blurred = ndimage.gaussian_filter(brain, sigma=5)
+least_blurred.shape
 plt.imshow(least_blurred)
 plt.show()
 plt.hist(least_blurred, bins=10)
+plt.show()
+less_blurred = ndimage.gaussian_filter(brain, sigma=10)
+less_blurred.shape
+plt.imshow(less_blurred)
+plt.show()
+plt.hist(less_blurred, bins = 10)
 plt.show()
