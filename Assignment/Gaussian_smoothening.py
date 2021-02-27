efd0149e7a2a9f283d259d4576eae4b3f08d0192
Abhinanda Dash <71942917+Abhi-mujhemein-kahin@users.noreply.github.com>
2021-02-27 10:30:31 +0530

    Smoothening is performed on reverse greyscale

--- Assignment/Gaussian_smoothening.py
+++ Assignment/Gaussian_smoothening.py
@@ -9,41 +9,41 @@ plt.hist(brain, bins = 10)
 plt.show()
 least_blurred = ndimage.gaussian_filter(brain, sigma=5)
 least_blurred.shape
-plt.imshow(least_blurred)
+plt.imshow(least_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(least_blurred, bins=10)
 plt.show()
 less_blurred = ndimage.gaussian_filter(brain, sigma=10)
 less_blurred.shape
-plt.imshow(less_blurred)
+plt.imshow(less_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(less_blurred, bins = 10)
 plt.show()
 
 three_blurred = ndimage.gaussian_filter(brain, sigma=20)
 three_blurred.shape
-plt.imshow(three_blurred)
+plt.imshow(three_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(three_blurred, bins = 10)
 plt.show()
 
 four_blurred = ndimage.gaussian_filter(brain, sigma=30)
 four_blurred.shape
-plt.imshow(four_blurred)
+plt.imshow(four_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(four_blurred, bins = 10)
 plt.show()
 
 five_blurred = ndimage.gaussian_filter(brain, sigma=40)
 five_blurred.shape
-plt.imshow(five_blurred)
+plt.imshow(five_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(five_blurred, bins = 10)
 plt.show()
 
 six_blurred = ndimage.gaussian_filter(brain, sigma=50)
 six_blurred.shape
-plt.imshow(six_blurred)
+plt.imshow(six_blurred, cmap='Greys_r')
 plt.show()
 plt.hist(six_blurred, bins = 10)
 plt.show()
