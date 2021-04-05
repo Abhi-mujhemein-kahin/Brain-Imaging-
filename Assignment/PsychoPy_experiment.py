68dc64faa78503b2406f85863a74812611141479
Abhinanda Dash <71942917+Abhi-mujhemein-kahin@users.noreply.github.com>
2021-04-05 23:33:40 +0530

    Basic Experiment information, eg version etc.

--- Assignment/PsychoPy_experiment.py
+++ Assignment/PsychoPy_experiment.py
@@ -6,7 +6,7 @@ from psychopy import sound, gui, visual, core, data, event, logging, clock, colo
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER) #Import important constants
 
-import numpy as np  # whole numpy lib is available, prepend 'np.'
+import numpy as np  # whole numpy lib is available
 from numpy import (sin, cos, tan, log, log10, pi, average,
                    sqrt, std, deg2rad, rad2deg, linspace, asarray)
 from numpy.random import random, randint, normal, shuffle, choice as randchoice
@@ -17,4 +17,15 @@ from psychopy.hardware import keyboard #import keyboard library
 # Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2021.1.2' #version of PsychoPy being used
+expName = 'dictator game'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001', 'Gender': ''} #Stores information about participant, session number and gender
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName) 
+if dlg.OK == False:
+    core.quit()  # If user presses cancel, experiment window will terminate
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
 
