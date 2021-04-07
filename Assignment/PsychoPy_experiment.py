edbc28f4ce6204cf760e1f68df04f44fc0e2efaa
Abhinanda Dash <71942917+Abhi-mujhemein-kahin@users.noreply.github.com>
2021-04-07 11:29:27 +0530

    Initializing various components of the experiment
    Eg; Instructions1, instructions2, Routine1, Routine2 etc.

--- Assignment/PsychoPy_experiment.py
+++ Assignment/PsychoPy_experiment.py
@@ -58,6 +58,103 @@ expInfo['frameRate'] = win.getActualFrameRate()
 if expInfo['frameRate'] != None:
     frameDur = 1.0 / round(expInfo['frameRate'])
 else:
-    frameDur = 1.0 / 60.0  # could not measure, so guess
+    frameDur = 1.0 / 60.0  # could not measure, so use the standard.
+    
+#--Initialize all the components for the experiment
+# create a default keyboard (e.g. to check for escape)
+defaultKeyboard = keyboard.Keyboard()
+
+# Initialize components for Routine "Instructions"
+InstructionsClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='Welcome to the Dictator game! Imagine that you have Rs 1000 with you, and you have the ability to give any part of this money to another player who is a part of the experiment. Any allocation made by you cannot be rejected or turned down by the other participants in the experiment. You can choose to keep or give away all of the money allocated to you. Please note that your total residual endowment will consist of whatever money is left after the decision you make. Press SPACE to continue or ESC to exit the game.',
+    font='Arial',
+    pos=(0, 0), height=0.03, wrapWidth=None, ori=0.0, 
+    color='lightgreen', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp = keyboard.Keyboard()
+
+# Initialize components for Routine "instructions2"
+instructions2Clock = core.Clock()
+text_6 = visual.TextStim(win=win, name='text_6',
+    text='The screen will display the name or number of the participant. A slider will be displayed which will contain denominations of 100 that can be given to the other participants of the study. You have to select the option which is consistent with the amount you want to allocate. Press SPACE if you want to continue to the game, or press ESC if you want to end the experiment. Press Space after every trial to move on to the next participant. It is assumed that you will have Rs 1000 for every new participant shown on the screen. The name of the participants will be visible to your for these trials.',
+    font='Arial',
+    units='norm', pos=(0, 0.3), height=0.08, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=1.0, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_2 = keyboard.Keyboard()
+
+# Initialize components for Routine "Trial1"
+Trial1Clock = core.Clock()
+text_2 = visual.TextStim(win=win, name='text_2',
+    text='',
+    font='Arial',
+    units='norm', pos=(0, 0.75), height=0.1, wrapWidth=None, ori=0.0, 
+    color='lightblue', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_3 = keyboard.Keyboard()
+slider = visual.Slider(win=win, name='slider',
+    size=(0.8, 0.5 ), pos=(0, -0.3), units='norm',
+    labels=(0,100,200,300,400,500,600,700,800,900,1000), ticks=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), granularity=1.0,
+    style='slider', styleTweaks=('triangleMarker',), opacity=1.0,
+    color='Darkblue', fillColor='Red', borderColor='White', colorSpace='rgb',
+    font='Arial', labelHeight=0.07,
+    flip=True, depth=-2, readOnly=False)
+
+# Initialize components for Routine "instructions2"
+instructions2Clock = core.Clock()
+text_6 = visual.TextStim(win=win, name='text_6',
+    text='The screen will display the name or number of the participant. A slider will be displayed which will contain denominations of 100 that can be given to the other participants of the study. You have to select the option which is consistent with the amount you want to allocate. Press SPACE if you want to continue to the game, or press ESC if you want to end the experiment. Press Space after every trial to move on to the next participant. It is assumed that you will have Rs 1000 for every new participant shown on the screen. The name of the participants will be visible to your for these trials.',
+    font='Arial',
+    units='norm', pos=(0, 0.3), height=0.08, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=1.0, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_2 = keyboard.Keyboard()
+
+# Initialize components for Routine "Please_wait"
+Please_waitClock = core.Clock()
+text_5 = visual.TextStim(win=win, name='text_5',
+    text='Please Wait',
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+
+# Initialize components for Routine "Trial2"
+Trial2Clock = core.Clock()
+text_3 = visual.TextStim(win=win, name='text_3',
+    text='',
+    font='Arial',
+    units='norm', pos=(0, 0.75), height=0.1, wrapWidth=None, ori=0.0, 
+    color='lightblue', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_4 = keyboard.Keyboard()
+slider_2 = visual.Slider(win=win, name='slider_2',
+    size=(0.8, 0.5), pos=(0, -0.3), units='norm',
+    labels=(0,100,200,300,400,500,600,700,800,900,1000), ticks=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), granularity=1.0,
+    style='slider', styleTweaks=('triangleMarker',), opacity=1.0,
+    color='Darkblue', fillColor='Red', borderColor='White', colorSpace='rgb',
+    font='Arial', labelHeight=0.05,
+    flip=True, depth=-2, readOnly=False)
+
+# Initialize components for Routine "Thanks"
+ThanksClock = core.Clock()
+text_4 = visual.TextStim(win=win, name='text_4',
+    text='THANKS FOR YOUR PARTICIPATION!',
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
 
