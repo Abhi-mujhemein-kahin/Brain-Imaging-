08ed26956ac0b04385bfd7fffc2d5cfdce2890ec
Abhinanda Dash <71942917+Abhi-mujhemein-kahin@users.noreply.github.com>
2021-04-09 15:20:53 +0530

    Experiment and participant data is collected

--- Assignment/Implicit_Association_test.py
+++ Assignment/Implicit_Association_test.py
@@ -18,3 +18,14 @@ from psychopy.hardware import keyboard
 # Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2021.1.2'
+expName = 'IAT-1.4'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'order': ['random', 1, 2], 'session': '001', 'gender': ['male', 'female', 'other']}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
